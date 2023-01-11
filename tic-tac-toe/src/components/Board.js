0bfad710a29b911e154f57d8c8d12295cfe4442c
Viktoriia Zubarieva <viktoria.dubinina@gmail.com>
2023-01-11 12:57:46 -0800

    render columns 3x3

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {Box} from "./Box"
+import "./Board.css"
 
 export const Board = ({board, onClick}) => {
     return (
-        <div>
+        <div className="board">
             {board.map((value, idx) => {
                 return<Box value={value} onClick={() => onClick(idx)}/>
             })}
