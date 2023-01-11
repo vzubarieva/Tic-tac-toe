42b63e547b5cb6c524e1f229b0315b07a83f86cd
Viktoriia Zubarieva <viktoria.dubinina@gmail.com>
2023-01-11 13:31:16 -0800

    add actial game functionality

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,11 +1,54 @@
+import React, {useState} from 'react';
 import './App.css';
 import {Board} from "./components/Board"
 
 function App() {
-  const board = ["X","X","X","X","X","X","X","X","X",]
+
+  const WIN_CONDITIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+ const [board, setBoard] = useState(Array(9).fill(null));
+ const [xPlaying, setXPlaying] = useState(true);
+
+ const handleBoxClick = (boxIdx) => {
+  const updatedBoard = board.map((value, idx) => {
+    if(idx === boxIdx){
+      return xPlaying === true ? "X" : "O";
+    } else {
+      return value;
+    }
+  })
+
+  checkWinner(updatedBoard);
+
+  setBoard(updatedBoard);
+
+  setXPlaying(!xPlaying);
+ }
+
+ const checkWinner = (board) => {
+    for(let i = 0; i < WIN_CONDITIONS.length; i++){
+      const [x,y,z] = WIN_CONDITIONS[i];
+
+      if(board[x] && board[x] === board[y] && board[y] === board[z]){
+        console.log(board[x])
+        return board[x];
+      }
+    }
+  }
+
+ 
   return (
     <div className="App">
-      <Board board={board} onClick={null} />
+      <Board board={board} onClick={handleBoxClick} />
     </div>
   );
 }
